fe531698a01f7363bc8b97b51166b58576614bbd
15980839993 <hhb7963254>
2022-01-12 01:02:47 +0800

    BaseEntity

---
+++ zoom_base/src/main/java/com/huanghongbe/zoom/base/entity/SuperEntity.java
@@ -0,0 +1,45 @@
+package com.huanghongbe.zoom.base.entity;
+
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.huanghongbe.zoom.base.enums.EStatus;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author ：huanghongbe
+ * @description：
+ * @date ：2022-01-12 0:38
+ */
+@Data
+public class SuperEntity<T extends Model> extends Model{
+    private static final long serialVersionUID = 1310878133376149230L;
+
+    /**
+     * 唯一UID
+     */
+    private String uid;
+
+    /**
+     * 状态 0：失效 1：生效
+     */
+    private int status;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    public SuperEntity(){
+        status= EStatus.ENABLED;
+        Date date=new Date();
+        createTime=date;
+        updateTime=date;
+    }
+}
---
+++ zoom_base/src/main/java/com/huanghongbe/zoom/base/enums/EStatus.java
@@ -0,0 +1,24 @@
+package com.huanghongbe.zoom.base.enums;
+
+/**
+ * @author ：huanghongbe
+ * @description：
+ * @date ：2022-01-12 0:53
+ */
+public class EStatus {
+    /**
+     * 删除的
+     */
+    public static final int DISABLED = 0;
+
+    /**
+     * 启用的
+     */
+    public static final int ENABLED = 0;
+
+    /**
+     * 冻结的
+     */
+    public static final int FREEZE = 0;
+
+}
